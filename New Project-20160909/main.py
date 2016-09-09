3332d0b3c9e56c7f236010d8165814fe4db9b1b7
codesmit <codesmit@users.noreply.github.com>
2016-09-09 19:28:31 +0530

    Created by Coding Ground Client

---
+++ New Project-20160909/main.py
@@ -0,0 +1,17 @@
+# Hello World program in Python
+    
+print "Algorithms and Data Sturctures\n"
+print "Start using Built in collection data Types "
+s= [1,34,True,24,3.23]
+print s
+d = [234,342,'aefr',2.34]
+print s + d
+print len(s + d)
+print s[0:4]
+s.append('excuse')
+print s
+s.pop(1)
+print s.sort()
+print range(10)
+s =[range(13)]
+print s
